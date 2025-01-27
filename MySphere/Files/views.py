f58c48a1bc9da267c6bebae50efb57ad3980a080
Mason <mason.n.dinosaur@gmail.com>
2025-01-26 20:31:22 -0600

    working with files

--- MySphere/Files/views.py
+++ MySphere/Files/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import os
+import glob
 
 # Create your views here.
 
 def home(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+
+    directory = "/home/mason-server/"
+
+    files = glob.glob(f"{directory}*")
+
+    serverFiles = []
+
+    for file in files:
+        if os.path.isdir(file):
+            serverFiles.append([file, True, file.removeprefix(directory)])
+
+        else:
+            serverFiles.append([file, False, file.removeprefix(directory)])
+
+
+    context = {
+        "files": serverFiles
+    }
+
+    return HttpResponse(template.render(context=context, request=request))
+
+def directory(request):
+    pass
