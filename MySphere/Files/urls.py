7bd27453eba981d609289ff837ff7fbd7436adc2
Mason <mason.n.dinosaur@gmail.com>
2025-01-26 20:53:32 -0600

    add new url path

--- MySphere/Files/urls.py
+++ MySphere/Files/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home-1'),
-    path('', views.home, name='home-2')
+    path('', views.home, name='home-2'),
+    path('home/<str:path>', views.directory, name='directory')
 ]
