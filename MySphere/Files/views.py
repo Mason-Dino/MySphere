f87aa78170122bff0dc959dca016a1fe3dc3f2b0
Mason <mason.n.dinosaur@gmail.com>
2025-01-26 21:04:26 -0600

    working on viewing directory

--- MySphere/Files/views.py
+++ MySphere/Files/views.py
@@ -29,5 +29,31 @@ def home(request):
 
     return HttpResponse(template.render(context=context, request=request))
 
-def directory(request):
-    pass
+def directory(request, path: str):
+    template = loader.get_template('dir.html')
+
+    root = "/home/mason-server/"
+    path = path.replace(".", "/")
+
+    #directory = root + path
+    directory = f"{root}{path}/"
+
+    files = glob.glob(f"{directory}*")
+
+    serverFiles = []
+
+    for file in files:
+        if os.path.isdir(file):
+            serverFiles.append([file, True, file.removeprefix(directory)])
+
+        else:
+            serverFiles.append([file, False, file.removeprefix(directory)])
+
+
+    context = {
+        "files": serverFiles,
+        "dir": directory
+    }
+
+    
+    return HttpResponse(template.render(context=context, request=request))
