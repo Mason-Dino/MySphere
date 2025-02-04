024c094d90acfa4944402ac7a84733356712d5d1
Mason <mason.n.dinosaur@gmail.com>
2025-02-04 09:23:59 -0600

    trying to load an image

--- MySphere/Files/urls.py
+++ MySphere/Files/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('home/', views.home, name='home-1'),
     path('', views.home, name='home-2'),
