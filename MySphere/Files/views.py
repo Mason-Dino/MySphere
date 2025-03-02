4673fe7b89f77b8657ab62f4c04c16b04618941e
Mason <mason.n.dinosaur@gmail.com>
2025-03-01 20:26:59 -0600

    added 2 new functions

--- MySphere/Files/views.py
+++ MySphere/Files/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import FileResponse
 from django.template import loader
 import os
 import glob
@@ -113,4 +114,24 @@ def viewTXT(request, path: str, file: str):
     return (HttpResponse(template.render(context=context, request=request)))
 
 def viewMovie(request, path: str, file: str):
-    pass
+    template = loader.get_template("view-movie.html")
+    
+    path = path.replace(".", "/")
+    
+    with open(f"{path}{file}", "rb") as video:
+        videoContent = video.read()
+        
+    context = {
+        "video": videoContent,
+        "filename": file
+    }
+    
+    return HttpResponse(template.render(context=context, request=request))
+
+def playMovie(request, path: str, file: str):
+    
+    path = path.replace(".", "/")
+    
+    video = open(f"{path}{file}", 'rb')
+        
+    return FileResponse(video, content_type='video/webm')
