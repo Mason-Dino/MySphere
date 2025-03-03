5f4957443112af67c99c6731ca847a32164c1ca4
Mason <mason.n.dinosaur@gmail.com>
2025-03-02 21:18:09 -0600

    add the url root url end point

--- MySphere/MySphere/urls.py
+++ MySphere/MySphere/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('files/', include("Files.urls")),
+    path('usb/', include('USB.urls')),
     path('admin/', admin.site.urls),
 ]
