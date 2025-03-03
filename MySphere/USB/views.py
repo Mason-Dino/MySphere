9528b9c432937f1bf226ca3a2ddd959e53311db5
Mason <mason.n.dinosaur@gmail.com>
2025-03-02 21:17:52 -0600

    starting to work with usb urls

--- MySphere/USB/views.py
+++ MySphere/USB/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import FileResponse
+from django.template import loader
+import os
+import glob
 
 # Create your views here.
+def home(requests):
+    template = loader.get_template('home.html')
+    print("he")
+    
+    return HttpResponse(template.render(request=requests))
