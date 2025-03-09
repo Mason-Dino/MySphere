58bb311634307b97b772f1e80cfb293dc02a7e2c
Mason <mason.n.dinosaur@gmail.com>
2025-03-09 17:04:13 -0500

    working on displaying all folders

--- MySphere/Folder/urls.py
+++ MySphere/Folder/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home/', views.home, name='home-1'),
     path('', views.home, name='home-2'),
-    path('make-folder/', views.makeFolder, name="make-folder")
+    path('make-folder/', views.makeFolder, name="make-folder"),
+    path('home/<str:path>', views.dir, name='dir-folder')
 ]
--- MySphere/Folder/views.py
+++ MySphere/Folder/views.py
@@ -12,7 +12,7 @@ import json
 # Create your views here.
 def home(requests):
     template = loader.get_template('home-folder.html')
-    logger = logging.getLogger("home")
+    logger = logging.getLogger("home-folder")
     logging.basicConfig(filename="viewTXT.log")
 
     os.scandir(os.getcwd())
@@ -20,13 +20,33 @@ def home(requests):
 
     for dir in glob.glob(f"/home/mason-server/*"):
         if os.path.isdir(dir) and dir != "/home/mason-server/usb":
-            directories.append([dir, dir.removeprefix("/home/mason-server/")])
+            directories.append([dir, dir.removeprefix("/home/mason-server/"), dir.removeprefix("/home/mason-server/").replace("/", ".")])
 
     logger.error(f"{directories}")
 
     context = {
         "directories": directories,
         "path": "/home/mason-server/"
+    }
+    
+    return HttpResponse(template.render(context=context, request=requests))
+
+def dir(requests, path: str):
+    template = loader.get_template('home-folder.html')
+    logger = logging.getLogger("dir-folder")
+    logging.basicConfig(filename="viewTXT.log")
+    directories = []
+    
+    path = path.replace(".", "/")
+    
+    for dir in glob.glob(f"/home/mason-server/{path}/*"):
+        if os.path.isdir(dir):
+            logger.error(f"{dir.removeprefix('/home/mason-server/').replace('/', '.')}")
+            directories.append([dir, dir.removeprefix(f"/home/mason-server/{path}/"), dir.removeprefix("/home/mason-server/").replace("/", ".")])
+    
+    context = {
+        "directories": directories,
+        "path": f"/home/mason-server/{path}"
     }
     
     return HttpResponse(template.render(context=context, request=requests))
