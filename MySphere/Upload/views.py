2e5846d59d2dbba2ed46d3499646cc83f0a6a7a0
Mason <mason.n.dinosaur@gmail.com>
2025-03-12 15:23:45 -0500

    fixing file upload

--- MySphere/Upload/views.py
+++ MySphere/Upload/views.py
@@ -1,32 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.http import FileResponse
 from django.template import loader
+from django import forms
 import os
 import glob
 
 import logging
 import json
 
+UPLOAD_DIR = "/home/mason-server/"
+
 # Create your views here.
 def home(requests):
     template = loader.get_template('home-upload.html')
     logger = logging.getLogger("home-upload")
     logging.basicConfig(filename="viewTXT.log")
-
     
     return HttpResponse(template.render(request=requests))
+    
+def handleUpload(f):
+    with open("/home/mason-sever/name.txt", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 def fileUpload(requests):
     logger = logging.getLogger("file-upload")
     logging.basicConfig(filename="viewTXT.log")
     
     if requests.method == "POST":
-        logger.error("TEst")
-        logger.error(f"file: {requests.FILES}")
-        logger.error(f"file: {requests.body}")
+        logger.error("check1")
+        logger.error(f"{requests.FILES}")
+        
+        uploadFile = requests.FILES['file']
+        
+        filePath = os.path.join(UPLOAD_DIR, uploadFile.name)
+
+        logger.error(f"File uploading: {filePath}")
+        
+        file = open(filePath, 'x')
+        file.close()
+        logger.error("check 2")
+        with open(filePath, "wb") as f:
+            for chunk in uploadFile.chunks():
+                f.write(chunk)
+                
+        logger.error(f"File uploaded: {filePath}")
+        
         
         return JsonResponse({
-            "code": 200
+            "code": 202
         })
