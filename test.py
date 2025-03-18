30a9679e98dbcd50f29ef7ecc49deac8df0e0486
Mason <mason.n.dinosaur@gmail.com>
2025-03-18 10:02:08 -0500

    testing usb mount point

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
+import glob
+import subprocess
 import os
-import shutil
 
-print(os.getcwd())
+files = glob.glob("/dev/sd*")
+usb = []
 
-mainFile = input("What file to copy from: ")
-whereFile = input("Where do you want to copy the file: ")
+for file in files:
+    usb.append(file.removeprefix('/dev/'))
 
-shutil.copyfile(mainFile, whereFile)
+
+for device in usb:
+    mount_point = f"/home/mason-server/usb-{device}"
+    
+    if not os.path.exists(mount_point):
+        os.makedirs(mount_point)
+
+# Mount each USB device
+for i, device in enumerate(usb):
+    mount_point = f"/home/mason-server/usb-{device}"
+    
+    # Mount the device
+    result = subprocess.call(["sudo", "mount", f"/dev/{device}", mount_point])
+    
+    if result == 0:
+        print(f"Successfully mounted /dev/{device} at {mount_point}")
+    else:
+        print(f"Failed to mount /dev/{device}")
+        os.removedirs(mount_point)
+
+# Print the list of USB devices found
+print("Found USB devices:", files)
