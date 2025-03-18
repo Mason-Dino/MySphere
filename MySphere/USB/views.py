3848af54175e938655ca6b05898371f397c0f307
Mason <mason.n.dinosaur@gmail.com>
2025-03-18 10:19:09 -0500

    make a getValidUSB function

--- MySphere/USB/views.py
+++ MySphere/USB/views.py
@@ -4,10 +4,65 @@ from django.http import FileResponse
 from django.template import loader
 import os
 import glob
+import subprocess
+import logging
 
 # Create your views here.
+def getValidUSB():
+    logger = logging.getLogger("getValidUSB")
+    logging.basicConfig(filename="viewTXT.log")
+    
+    files = glob.glob("/dev/sd*")
+    logger.error(f"usb device: {files}")
+    
+    usb = []
+    validUSB = []
+
+    for file in files:
+        usb.append(file.removeprefix('/dev/'))
+
+
+    for device in usb:
+        mount_point = f"/home/mason-server/usb-{device}"
+        
+        if not os.path.exists(mount_point):
+            os.makedirs(mount_point)
+
+    # Mount each USB device
+    for i, device in enumerate(usb):
+        mount_point = f"/home/mason-server/usb-{device}"
+        
+        # Mount the device
+        result = subprocess.call(["sudo", "mount", f"/dev/{device}", mount_point])
+        
+        if result == 0:
+            logger.error(f"Successfully mounted /dev/{device} at {mount_point}")
+            result = subprocess.call(["sudo", "umount", mount_point])
+            
+            if result == 0:
+                try:
+                    os.removedirs(mount_point)
+                except:
+                    pass
+            
+            validUSB.append(device)
+            logger.error(validUSB)
+        else:
+            print(f"Failed to mount /dev/{device}")
+            try:
+                os.removedirs(mount_point)
+            except:
+                pass
+            
+    return validUSB
+
+
+
 def home(requests):
     template = loader.get_template('home-usb.html')
-    print("he")
     
-    return HttpResponse(template.render(request=requests))
+    context = {
+        "usb": getValidUSB()
+    }
+    
+    return HttpResponse(template.render(context=context, request=requests))
