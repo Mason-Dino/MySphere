dd75ebb002860e1ecd6eb07d7108fc55d814aa58
Mason <mason.n.dinosaur@gmail.com>
2025-03-18 20:49:52 -0500

    working on edit page

--- MySphere/Editor/views.py
+++ MySphere/Editor/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import FileResponse
+from django.template import loader
+import os
+import glob
+import subprocess
+import logging
+import json
 
 # Create your views here.
+def home(requests):
+    template = loader.get_template('home-edit.html')
+    
+    return HttpResponse(template.render(request=requests))
